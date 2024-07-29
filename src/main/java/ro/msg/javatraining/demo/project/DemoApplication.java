0733993593c14ef678b0f254d8c86ef54b0648ec
Bogdan Stupariu <bogdan.stupariu@msg.group>
2024-07-29 09:52:33 +0300

    Name Change

--- src/main/java/ro/msg/javatraining/demo/project/DemoApplication.java
+++ src/main/java/ro/msg/javatraining/demo/project/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JwtpocApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JwtpocApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
