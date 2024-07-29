6b2342890c776eddfed931f20099356eddd558d0
Bogdan Stupariu <bogdan.stupariu@msg.group>
2024-07-29 13:28:12 +0300

    Feature Email-Sender implemented Need to add ticket sending string to it

--- src/main/java/ro/msg/javatraining/demo/project/controller/app/EmailController.java
+++ src/main/java/ro/msg/javatraining/demo/project/controller/app/EmailController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ro.msg.javatraining.demo.project.dto.EmailDetailsDto;
 import ro.msg.javatraining.demo.project.model.Email;
 import ro.msg.javatraining.demo.project.model.User;
+import ro.msg.javatraining.demo.project.service.AirportService;
 import ro.msg.javatraining.demo.project.service.EmailService;
 
 @RestController
@@ -18,12 +19,15 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
+    @Autowired
+    private AirportService airportService;
+
     @PostMapping("/send-email")
     @Secured(value = {"ROLE_USER", "ROLE_ADMIN"})
 
     public String sendEmail(@RequestBody EmailDetailsDto emailDetails) {
         try {
-            emailService.sendSimpleMessage(emailDetails.getTo(), emailDetails.getSubject(), emailDetails.getText());
+            emailService.sendAirplaneDetailsMessage(emailDetails.getTo(), emailDetails.getSubject(), emailDetails.getText());
             return "Email sent successfully";
         } catch (Exception e) {
             e.printStackTrace();
